7be33d31e7d422f2774133ce49529ce845249274
DESKTOP-R3B7RCC\RBT <kousalya@farmwiseai.com>
2023-07-06 14:13:37 +0530

    Chat activity design and dummy adapter added

--- app/src/main/java/com/fai/agribot/ProfileActivity.java
+++ app/src/main/java/com/fai/agribot/ProfileActivity.java
@@ -2,13 +2,28 @@ package com.fai.agribot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
-
+EditText txtname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Button btnsave= findViewById(R.id.btnSave);
+        txtname= findViewById(R.id.txtName);
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+                intent.putExtra("name", txtname.getText().toString());
+                startActivity(intent);
+            }
+        });
+
     }
 }
